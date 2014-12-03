0b8dfd87d057d617f28e128630ce6e2a730333b6
Anderson.Makiyama <Anderson.Makiyama@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-03 01:06:16 +0000

    Publicação
    git-svn-id: https://plugins.svn.wordpress.org/hot-login/tags/1.0@1037187 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/options.php
@@ -0,0 +1,206 @@
+<div class="wrap">
+<div class="icon32"><img src='<?php echo plugins_url('/images/icon-32.png', dirname(__FILE__))?>' /></div>
+        
+<h2>Hot Login</h2>
+    
+  		<table width="100%"><tr>
+
+        <td style="vertical-align:top">
+ 
+ 		<form action="" method="post" class="form-table">
+        		<?php
+                 wp_nonce_field('update',self::CLASS_NAME);
+				?>
+                
+                <h2>Configurações do Hotconnect</h2>
+        <div class="metabox-holder">         
+		<div class="postbox" >
+        
+        	<div class="inside">
+            
+            	<table>
+                <tr>
+            	
+                <th scope="row">
+                <label>ID da Aplicação</label>
+                </th >
+                <td>
+                <input type="text" name="app_id" value="<?php echo $options['app_id']?>" class="regular-text" /> <p class="description">( Digite o ID da aplicação criada no Hotmart )</p>
+                </td>
+                </tr>
+                <tr>
+                <th scope="row">
+                 <label>Código Secreto da Aplicação</label>
+                 </th>
+                 <td>
+                 <input type="text" name="app_secret" class="regular-text" value="<?php echo $options['app_secret']?>" /><p class="description">( Digite o Código Secreto da Aplicação )</p>
+                </td>
+                </tr>
+                <tr>
+                <th scope="row">
+                <label>ID do Produto</label>
+                </th>
+                <td><input type="text" name="id_produto" size="20" value="<?php echo $options['id_produto']?>" /> <p class="description">( Digite o ID do seu Produto )</p>
+                </td>
+                </tr>
+                </table>
+               
+                <input type="submit" name="submit" value="Salvar Todas as Alterações" class="button-primary" />
+                
+                
+
+			</div>
+		</div>
+        </div>
+			
+            <h2>Outras Configurações</h2>
+
+        <div class="metabox-holder">         
+		<div class="postbox" >
+        
+        	<div class="inside">
+            
+            	<table>
+                <tr>
+            	
+                <th scope="row">
+                <label>Página Inicial do Comprador</label>
+                </th >
+                <td>
+                <input type="text" name="pagina_inicial" value="<?php echo $options['pagina_inicial']?>" class="regular-text" /> <p class="description">( Digite o URL da página Inicial que deve ser exibida ao comprador após ele fazer Login )</p>
+                </td>
+                </tr>
+                </table>
+               
+                <input type="submit" name="submit" value="Salvar Todas as Alterações" class="button-primary" />
+                
+                
+
+			</div>
+		</div>
+        </div>
+        
+                
+        </form>
+          
+   		</td>
+        <td style="vertical-align:top; width:410px">
+
+
+
+         <div class="metabox-holder">
+
+		<div class="postbox" >    
+        
+      		 <h3 style="font-size:24px; text-transform:uppercase;color:#F00;">
+
+        	Gostou do Plugin? Fique Atualizado!
+
+            </h3>       
+
+            <h3>Cadastre seu Email e Seja o primeiro a ficar sabendo sobre novos plugins meus Grátis</a></h3>
+
+            
+
+        	<div class="inside">
+
+                <p>
+
+                <!-- BEGIN TrafficWave.net AutoResponder Capture Form - BUILD 090412-->
+                <div id="TRWVLCPDiv" style="text-align: left; width: 400px;"><form id="TRWVLCPForm" name="TRWVLCPForm" method="POST" action="http://www.trafficwave.net/cgi-bin/autoresp/inforeq.cgi" target="_blank"><input type=hidden name="trwvid" value="ganhardinheiro"><input type=hidden name="series" value="dosplugins"><input type=hidden name="subscrLandingURL" value=""><input type=hidden name="confirmLandingURL" value=""><table class="LCPdataForm" style="width: 100%;"><tr><td width=25% ></td><td ><input id="da_name" name="da_name" value="amigo" type=hidden style="width: 100%"></td></tr> <tr><td width=25% >Seu Email</td><td ><input id="da_email" name="da_email" type=text style="width: 100%" placeholder="... Seu Email Aqui" required="required"></td></tr> </table><input type="submit" id="TRWVSubM"  value="Quero Receber" class="button-primary" /></form><div align="center" style="font-size: 11px; padding:2px;"><a href="http://www.trafficwave.net/members/ganhardinheiro" target="_blank" rel="external nofollow" title="Email Marketing">Email Marketing by TrafficW</a></div></div>
+                
+                <!-- END TrafficWave.net AR Capture Form - BUILD 090412-->
+
+
+				</p>
+
+
+
+			</div> 
+
+                        
+
+		</div>
+
+        </div>
+        
+        
+        <div class="metabox-holder">
+
+		<div class="postbox" >
+
+        
+
+        	<h3 style="font-size:24px; text-transform:uppercase;color:#F00;">
+
+        	Pague 1, Leve 87!
+
+            </h3>
+
+            
+
+             <h3>Super Pacote de Temas Premium: <a href="http://plugin-wp.net/elegantthemes" target="_blank">Elegant Themes</a></h3>
+
+             
+
+        	<div class="inside">
+
+                <p>
+
+                <a href="http://plugin-wp.net/elegantthemes" target="_blank"><img src="<?php echo $anderson_makiyama[self::PLUGIN_ID]->plugin_url?>images/elegantthemes.jpg" ></a>
+
+				</p>
+
+				<a href="http://plugin-wp.net/elegantthemes" target="_blank" class="button-primary" rel="external nofollow" >Quero Conhecer</a>
+				
+
+			</div>
+
+ 
+ 		</div>
+        </div>
+        
+
+
+              
+
+       </td>
+       </tr>
+       </table>
+
+
+<hr />
+
+
+<table>
+<tr>
+<td>
+<img src="<?php echo $anderson_makiyama[self::PLUGIN_ID]->plugin_url?>images/anderson-makiyama.png" />
+</td>
+<td>
+<ul>
+
+<li>Autor: <strong>Anderson Makiyama</strong>
+
+</li>
+
+<li>Email do Autor: <a href="mailto:andersonmaki@gmail.com" target="_blank">andersonmaki@gmail.com</a>
+
+</li>
+
+<li>Página do Plugin: <a href="<?php echo self::PLUGIN_PAGE?>" target="_blank"><?php echo self::PLUGIN_PAGE?></a>
+
+</li>
+
+<li>
+
+Página do Autor: <a href="http://ganhardinheiroblog.net" target="_blank">Ganhar Dinheiro Blog</a>
+
+</li>
+
+</ul>
+</td>
+</tr>
+</table>
+
+</div>
